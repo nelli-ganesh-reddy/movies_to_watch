c17764165c2574b74377c965c1187bbb2f89e2d1
nelli-ganesh-reddy <ng24csb1a45@student.nitw.ac.in>
2026-05-10 16:05:08 +0000

    .

--- app.py
+++ app.py
@@ -107,4 +107,5 @@ def delete_movie(movie_id):
     return jsonify({"message" : "movie deleted !"}), 200
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
